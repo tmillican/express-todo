d04d542224159c65357c96179aad4fc5b0f7a8da
Travis Millican <travisjmillican@gmail.com>
2018-09-13 18:55:25 -0500

    Add ExpressJS

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import http from 'http';
+import express from 'express';
+const app = express();
 
-const server = http.createServer((req, res) => {
+app.get('/', function(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello world!');
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
   console.log('Server running at http://localhost:3000');
 });

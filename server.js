55c9aa63f0c027feea0b753853b880d8b96ff687
Travis Millican <travisjmillican@gmail.com>
2018-09-13 19:06:32 -0500

    Add ejs and stub index view

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import http from 'http';
 import express from 'express';
 const app = express();
 
+app.set('view engine', 'ejs');
+app.locals.layout = false;
+
+app.use('/public', express.static('public'));
+
 app.get('/', function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world!');
+  res.render('index');
 });
 
 app.listen(3000, '127.0.0.1', () => {
